545bafbcb2ac2a2c701c1a5c9c71d9efee67564e
bjlixiaopeng <bjlixiaopeng@corp.netease.com>
2018-07-18 20:06:03 +0800

    数据库脚本提交

--- platform-admin/src/main/resources/db.sql
+++ platform-admin/src/main/resources/db.sql
@@ -59,3 +59,40 @@ updator INT COMMENT '最后修改人',
 PRIMARY KEY(id),
 KEY idx_material_type_name(material_type_name)
 )ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='物品类型表';
+
+
+DROP TABLE IF EXISTS t_qr_code_apply;
+CREATE TABLE t_qr_code_apply(
+id INT NOT NULL AUTO_INCREMENT COMMENT '主键Id',
+qr_code_type TINYINT COMMENT '二维码类型（0：物品二维码；1：企业二维码）',
+batch_no VARCHAR(30) COMMENT '申请批次号',
+quantity INT NOT NULL COMMENT '二维码生成数量',
+prefix VARCHAR(20) COMMENT '二维前缀',
+applicant INT(22) COMMENT '申请人',
+apply_date DATETIME DEFAULT NOW() COMMENT '申请时间',
+qr_code_status TINYINT COMMENT '状态（0：审核中；1：已发放）',
+reject_reason VARCHAR(100) COMMENT '驳回原因',
+generate_man INT COMMENT '生成人员',
+generate_date DATETIME COMMENT '生成时间',
+bind_man INT COMMENT '绑定人员',
+bind_date DATETIME COMMENT '绑定时间',
+matetial_id INT COMMENT '物料Id',
+material_type_id INT COMMENT '物品类型',
+create_date DATETIME COMMENT '创建时间',
+PRIMARY KEY(id)
+)ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='二维码申请表';
+
+
+DROP TABLE IF EXISTS t_check_specific;
+CREATE TABLE t_check_specific(
+id INT NOT NULL AUTO_INCREMENT COMMENT '主键Id',
+specific_name VARCHAR(30) NOT NULL COMMENT '规范名称',
+remark VARCHAR(100) COMMENT '备注',
+check_item VARCHAR(2000) COMMENT '检查项',
+create_time DATETIME COMMENT '创建时间',
+creator INT COMMENT '创建者',
+update_time DATETIME COMMENT '最后修改人',
+updator INT COMMENT '最后修改人',
+PRIMARY KEY(id),
+KEY idx_specific_name(specific_name)
+)ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='检查规范表';
