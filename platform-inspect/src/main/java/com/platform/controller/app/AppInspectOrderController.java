b599ecc2340e647002e3d1cc84aae3346a758770
bjlixiaopeng <bjlixiaopeng@corp.netease.com>
2018-09-01 18:06:20 +0800

    任务详情接口

--- platform-inspect/src/main/java/com/platform/controller/app/AppInspectOrderController.java
+++ platform-inspect/src/main/java/com/platform/controller/app/AppInspectOrderController.java
@@ -1,13 +1,21 @@
 package com.platform.controller.app;
 
+import com.platform.entity.AppUserEntity;
 import com.platform.entity.inspect.InspectOrderEntity;
+import com.platform.service.common.CommonService;
 import com.platform.service.inspect.IInspectOrderService;
+import com.platform.utils.PageUtils;
+import com.platform.utils.Query;
 import com.platform.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA
@@ -26,11 +34,33 @@ public class AppInspectOrderController {
     @Autowired
     private IInspectOrderService inspectOrderService;
 
+    @Autowired
+    private CommonService commonService;
+
     @RequestMapping(value = "/material/check",method = RequestMethod.POST)
     public R materialCheck(@ModelAttribute InspectOrderEntity entity){
         try {
             inspectOrderService.materialCheck(entity);
             return R.succeed();
+        } catch (Exception e) {
+            return R.failure();
+        }
+    }
+
+    /**
+     * 任务详情
+     * @return
+     */
+    @RequestMapping(value = "/task/detail")
+    public R taskDetail(){
+        try {
+            AppUserEntity appUser = commonService.getCurrentLoginUser();
+            Map<String,Object> map = new HashMap<String, Object>();
+            map.put("userId",appUser.getId());
+            Query query = new Query(map);
+            List<InspectOrderEntity> list = inspectOrderService.queryList(map);
+            Integer total = inspectOrderService.queryTotal(query);
+            return R.succeed().put("page",new PageUtils(list, total, query.getLimit(), query.getPage()));
         } catch (Exception e) {
             return R.failure();
         }
